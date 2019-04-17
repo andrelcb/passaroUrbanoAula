10f841fa3f2e13bf3cb5f2c6ad0ee47fade0ad23
Andre Leonardo Correa Barbosa <andre.barbosa@avantibrpremium.local>
2019-04-17 16:44:28 -0300

    okay

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
